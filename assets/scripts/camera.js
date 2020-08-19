b4a5f0a3eb21e7b5f0c40b38c5f5ba79bf954b4f
Fabio Andrei Lorentz <tifabio@gmail.com>
2020-08-18 22:24:23 -0300

    Map02

---
+++ assets/scripts/camera.js
@@ -0,0 +1,25 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        Hero: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    update (dt) {
+        let heroPosition = this.Hero.getPosition();
+        let cameraPosition = this.node.getPosition();
+        cameraPosition.lerp(heroPosition, 0.1, cameraPosition);
+        this.node.setPosition(0, cameraPosition.y);
+    },
+});
